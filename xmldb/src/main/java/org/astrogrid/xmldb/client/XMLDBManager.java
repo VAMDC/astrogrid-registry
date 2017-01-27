6262f32e21a88ef8405fd66a4c4008829543b359
Guy Rixon <guyrixon@gmail.com>
2017-01-27 15:22:16 +0000

    The project is rearranged to make it build.

--- xmldb/src/main/java/org/astrogrid/xmldb/client/XMLDBManager.java
+++ xmldb/src/main/java/org/astrogrid/xmldb/client/XMLDBManager.java
@@ -2,43 +2,14 @@ package org.astrogrid.xmldb.client;
 
 import org.astrogrid.config.Config;
 
-import java.io.IOException;
-import java.io.UnsupportedEncodingException;
-import javax.xml.parsers.ParserConfigurationException;
-import org.xml.sax.SAXException;
-import java.net.MalformedURLException;
-
-import java.net.URL;
-import java.net.HttpURLConnection;
-import java.net.URLEncoder;
-import org.astrogrid.util.DomHelper;
-import org.apache.axis.AxisFault;
-
-import java.io.DataOutputStream;
-
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-
-import org.w3c.dom.Document;
-import org.w3c.dom.NodeList;
-import org.w3c.dom.Element;
-import org.w3c.dom.Node;
-
-import org.xmldb.api.modules.XQueryService;
-import org.xmldb.api.modules.XPathQueryService;
-import org.xmldb.api.modules.XUpdateQueryService;
-import org.xmldb.api.base.Service;
 
 import org.exist.xmldb.DatabaseInstanceManager;
 import org.xmldb.api.DatabaseManager;
 import org.xmldb.api.base.Collection;
 import org.xmldb.api.base.Database;
-import org.xmldb.api.base.ResourceSet;
-import org.xmldb.api.modules.XMLResource;
-import org.xmldb.api.modules.CollectionManagementService;
-import org.xmldb.api.base.Resource;
 import org.xmldb.api.base.XMLDBException;
-import javax.xml.transform.OutputKeys;
 
 import java.util.Properties;
 import java.util.Enumeration;
@@ -135,6 +106,6 @@ public class XMLDBManager {
             DatabaseInstanceManager mgr = (DatabaseInstanceManager)coll.getService("DatabaseInstanceManager", "1.0");
             mgr.shutdown();
             setDBInitialized(false);
-            log.info("databae shutdown");
+            log.info("database shutdown");
     }
 }
